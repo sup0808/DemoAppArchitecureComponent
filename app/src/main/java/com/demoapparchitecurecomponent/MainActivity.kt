1b369e1a20481da3c8a4cf235bdfb2b1981170c7
Gupta <2174765@cognizant.com>
2024-02-19 23:46:10 +0530

    Implement Quote app

--- app/src/main/java/com/demoapparchitecurecomponent/MainActivity.kt
+++ app/src/main/java/com/demoapparchitecurecomponent/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.demoapparchitecurecomponent
 
+import android.app.Notification.Action
+import android.content.Intent
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
@@ -9,12 +11,15 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View.OnClickListener
+import androidx.lifecycle.ViewModelProvider
 import com.demoapparchitecurecomponent.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var mainViewModel: MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,8 +27,32 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        mainViewModel = ViewModelProvider(this, MainViewModelFactory(applicationContext))[MainViewModel::class.java]
 
+        setQuote(mainViewModel.getQuote())
+        binding.btnNext.setOnClickListener{
+            setQuote(mainViewModel.nextQuote())
+        }
+
+        binding.btnPrevious.setOnClickListener(previousClick)
+        binding.btnShare.setOnClickListener{
+            val intent = Intent(Intent.ACTION_SEND)
+            intent.type= "text/plain"
+            intent.putExtra(Intent.EXTRA_TEXT, mainViewModel.getQuote().text)
+            startActivity(intent)
+        }
     }
+
+    private val previousClick : OnClickListener = OnClickListener {
+        setQuote(mainViewModel.previosQuote())
+    }
+
+    private fun setQuote(quote : Quotes){
+        binding.txtTitle.text = quote.text
+        binding.txtWriter.text = quote.author
+    }
+
+
 
 
 }
---
+++ app/src/main/java/com/demoapparchitecurecomponent/Quotes.kt
@@ -0,0 +1,3 @@
+package com.demoapparchitecurecomponent
+
+data class Quotes(val text : String, val author : String)
