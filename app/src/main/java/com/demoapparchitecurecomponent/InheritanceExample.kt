b416cc335e8853b59666042e5d4a780e0d58e564
Gupta <2174765@cognizant.com>
2024-02-19 19:08:23 +0530

    Add example of Inheritance

---
+++ app/src/main/java/com/demoapparchitecurecomponent/InheritanceExample.kt
@@ -0,0 +1,45 @@
+package com.demoapparchitecurecomponent
+
+class InheritanceExample {
+}
+fun main() {
+    var user: User= Student()
+    user.getUserName()
+}
+
+open class User(val name : String) {
+    open fun getUserName(){
+        println("Base class :: User $name")
+    }
+}
+
+interface test{
+    fun getUserName(){
+        println("intercae :: dervied")
+    }
+}
+
+interface test2{
+    fun getUserName(){
+        println("intercae :: derviednnn")
+    }
+}
+
+class Student : User("supriya"), test,test2{
+    override fun getUserName(){
+        super<User>.getUserName()
+        super<test>.getUserName()
+        super<test2>.getUserName()
+        println("Derived class : Student")
+    }
+}
+
+
+
+
+
+/*
+*
+* Base class :: User supriya
+intercae :: dervied
+Derived class : Student*/
