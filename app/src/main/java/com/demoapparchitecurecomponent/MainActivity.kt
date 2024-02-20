dd7d4ab7aaef3640ae135b99d366cbefad8632cb
Gupta <2174765@cognizant.com>
2024-02-20 20:51:46 +0530

    Implement Databinding example

--- app/src/main/java/com/demoapparchitecurecomponent/MainActivity.kt
+++ app/src/main/java/com/demoapparchitecurecomponent/MainActivity.kt
@@ -9,18 +9,23 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
+import androidx.databinding.DataBindingUtil
 import com.demoapparchitecurecomponent.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
 
-    private lateinit var binding: ActivityMainBinding
+   private lateinit var binding : ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         setContentView(binding.root)
+
+        var quotesObj = Quotes("\"Do or do that you want\"","Supriya Gupta")
+
+       binding.quotes = quotesObj
 
 
     }
---
+++ app/src/main/java/com/demoapparchitecurecomponent/Quotes.kt
@@ -0,0 +1,3 @@
+package com.demoapparchitecurecomponent
+
+data class Quotes (val quote: String, val author :String)
