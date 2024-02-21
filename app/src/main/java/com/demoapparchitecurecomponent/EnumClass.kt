26b50a8e75b4ad2476d9696da1b1cbb804166c52
Gupta <2174765@cognizant.com>
2024-02-21 14:16:47 +0530

    Method inside Enum class example

--- app/src/main/java/com/demoapparchitecurecomponent/EnumClass.kt
+++ app/src/main/java/com/demoapparchitecurecomponent/EnumClass.kt
@@ -2,20 +2,16 @@ package com.demoapparchitecurecomponent
 
 fun main() {
     val currentDirections = Directions.NORTH
-
-    println(currentDirections)
-    println(currentDirections.name)
-    println(currentDirections.number)
-
-    for( i :Directions in Directions.values() ){
-        println(i)
-    }
-
+    currentDirections.printFormattedDirection()
 }
 
 enum class Directions(val number : Int) {
     EAST(1),
     WEST(2),
     NORTH(3),
-    SOUTH(4)
+    SOUTH(4); // put ; for wtiting function inside enum class
+
+    fun printFormattedDirection(){
+        println("Direction is $this")
+    }
 }
