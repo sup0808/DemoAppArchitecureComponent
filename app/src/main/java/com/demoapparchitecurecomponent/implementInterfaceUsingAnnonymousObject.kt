423959fccaac9fa5852812b9e9f9178914d36ed4
Gupta <2174765@cognizant.com>
2024-02-23 15:28:28 +0530

    Implemeting interfcae without inherit class and use annonyous object

---
+++ app/src/main/java/com/demoapparchitecurecomponent/implementInterfaceUsingAnnonymousObject.kt
@@ -0,0 +1,15 @@
+package com.demoapparchitecurecomponent
+
+fun main(){
+    val studentClass = object  : CLoneable(){
+        override fun clone() {
+
+            println("Interafce using ANnonyuous")
+        }
+    }
+    studentClass.clone()
+}
+
+interface CLoneable{
+    fun clone()
+}
