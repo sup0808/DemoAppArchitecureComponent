3a595192426005d872c5a055a0d531bbaf04f47f
Gupta <2174765@cognizant.com>
2024-02-28 12:08:02 +0530

    Extension function

---
+++ app/src/main/java/com/demoapparchitecurecomponent/extensionFunction.kt
@@ -0,0 +1,10 @@
+package com.demoapparchitecurecomponent
+fun main() {
+    println("SUpriya Gupta".test())
+
+}
+
+
+fun String.test() : String{
+    return "-------------\n$this\n---------------------"
+}
