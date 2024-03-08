ba4b611c313a2ad6bad24c0093ecce74d9663ab8
Gupta <2174765@cognizant.com>
2024-03-08 12:37:29 +0530

    Implement navigation component and add and delete fragment, popup and popupexclusive

---
+++ app/src/main/java/com/example/kotlinroomdatabase/frgament/home/HomeFragment.kt
@@ -0,0 +1,37 @@
+package com.example.kotlinroomdatabase.frgament.home
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.kotlinroomdatabase.R
+import com.example.kotlinroomdatabase.databinding.FragmentHomeBinding
+
+
+class HomeFragment : Fragment() {
+
+    lateinit var binding: FragmentHomeBinding
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        // Inflate the layout for this fragment
+        binding = FragmentHomeBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.textRedirect.setOnClickListener {
+            findNavController().navigate(
+                R.id.action_homeFragment_to_addFragment)
+        }
+    }
+
+}
