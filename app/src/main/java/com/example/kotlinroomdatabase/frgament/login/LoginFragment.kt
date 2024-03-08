5897deb1ad378abca5c4e7a79abce45787091cfc
Gupta <2174765@cognizant.com>
2024-03-08 13:54:32 +0530

    Passing data to destination using safeargs

--- app/src/main/java/com/example/kotlinroomdatabase/frgament/login/LoginFragment.kt
+++ app/src/main/java/com/example/kotlinroomdatabase/frgament/login/LoginFragment.kt
@@ -5,17 +5,34 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
 import com.example.kotlinroomdatabase.R
+import com.example.kotlinroomdatabase.databinding.FragmentLoginBinding
 
 
 class LoginFragment : Fragment() {
 
+    lateinit var  binding: FragmentLoginBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        binding = FragmentLoginBinding.inflate(inflater, container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.button.setOnClickListener{
+            val action = LoginFragmentDirections.actionLoginFragmentToWelcomeFragment(
+                binding.userName.text.toString(),
+                binding.password.text.toString())
+                findNavController().navigate(action)
+         //   findNavController().navigate(R.id.action_loginFragment_to_welcomeFragment)
+        }
+
     }
 
 }
--- app/src/main/java/com/example/kotlinroomdatabase/frgament/welcome/WelcomeFragment.kt
+++ app/src/main/java/com/example/kotlinroomdatabase/frgament/welcome/WelcomeFragment.kt
@@ -5,17 +5,32 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.navArgs
 import com.example.kotlinroomdatabase.R
+import com.example.kotlinroomdatabase.databinding.FragmentLoginBinding
+import com.example.kotlinroomdatabase.databinding.FragmentLoginBindingImpl
+import com.example.kotlinroomdatabase.databinding.FragmentWelcomeBinding
 
 
 class WelcomeFragment : Fragment() {
 
+    lateinit var binding: FragmentWelcomeBinding
+
+    val args: WelcomeFragmentArgs by navArgs()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_welcome, container, false)
+        binding= FragmentWelcomeBinding.inflate(inflater, container, false)
+        return  binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.txtUserName.text = args.username
+        binding.txtPassword.text = args.password
     }
 
 }
