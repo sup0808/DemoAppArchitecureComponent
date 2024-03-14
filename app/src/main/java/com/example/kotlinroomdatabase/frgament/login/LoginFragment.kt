c837fca64a72a927faaef8752a63bfe42165000f
Gupta <2174765@cognizant.com>
2024-03-14 12:08:19 +0530

    Deeplink implementation

--- app/src/main/java/com/example/kotlinroomdatabase/frgament/login/LoginFragment.kt
+++ app/src/main/java/com/example/kotlinroomdatabase/frgament/login/LoginFragment.kt
@@ -1,17 +1,22 @@
 package com.example.kotlinroomdatabase.frgament.login
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.text.Editable
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import com.example.kotlinroomdatabase.R
 import com.example.kotlinroomdatabase.databinding.FragmentLoginBinding
 
 
 class LoginFragment : Fragment() {
+
+     val args: LoginFragmentArgs by navArgs()
 
     lateinit var  binding: FragmentLoginBinding
     override fun onCreateView(
@@ -23,11 +28,15 @@ class LoginFragment : Fragment() {
         return binding.root
     }
 
+    @SuppressLint("SuspiciousIndentation")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.edUserName.text = args.username as Editable
+
+
         binding.button.setOnClickListener{
             val action = LoginFragmentDirections.actionLoginFragmentToWelcomeFragment(
-                binding.userName.text.toString(),
+                binding.edUserName.text.toString(),
                 binding.password.text.toString())
                 findNavController().navigate(action)
          //   findNavController().navigate(R.id.action_loginFragment_to_welcomeFragment)
