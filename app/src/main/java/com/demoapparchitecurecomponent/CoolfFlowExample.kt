0badd64529d24d3f0aa2bd733a8880c9b0a0371d
supriya gupta <supriyag75@gmail.com>
2024-06-01 17:49:21 +0530

    Flow is cold (Example) Flow does not emit value until it is not collected

---
+++ app/src/main/java/com/demoapparchitecurecomponent/CoolfFlowExample.kt
@@ -0,0 +1,24 @@
+package com.demoapparchitecurecomponent
+
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.runBlocking
+
+fun Simple() : Flow<Int> = flow {
+    println("Flow started")
+    for(i in 1..3){
+        delay(100)
+        emit(i)
+    }
+}
+
+fun main()= runBlocking {
+    println("Calling simple function...")
+    val flow = Simple()
+    println("Calling collect...")
+    flow.collect{ println(it) }
+    println("Calling collect again ...")
+    flow.collect{ println(it) }
+
+}
