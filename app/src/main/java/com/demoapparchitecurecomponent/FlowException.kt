59028eb7f99b51656c1a175b6c8b092902901dfb
supriya gupta <supriyag75@gmail.com>
2024-06-02 07:56:28 +0530

    Flow Exception using check(bollean)

--- app/src/main/java/com/demoapparchitecurecomponent/FlowException.kt
+++ app/src/main/java/com/demoapparchitecurecomponent/FlowException.kt
@@ -2,22 +2,27 @@ package com.demoapparchitecurecomponent
 
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.runBlocking
 
-fun FlowException() : Flow<Int> = flow {
-    for (i in 1..3) {
-        println("Emitting $i")
-        emit(i) // emit next value
+fun FlowException() : Flow<String> =
+    flow {
+        for (i in 1..3) {
+            println("Emitting $i")
+            emit(i) // emit next value
+        }
     }
-}
+        .map { value ->
+            check(value <= 1) { "Crashed on $value" }
+            "string $value"
+        }
 
 fun main() = runBlocking {
     try{
+
         FlowException().collect{
             println(it)
-            check(it<=1){
-                "collected : $it"
-            }
+
         }
     }
     catch (e : Throwable){
@@ -30,3 +35,10 @@ Emitting 1
 Emitting 2
 2
 Caught :: java.lang.IllegalStateException: collected : 2*/
+
+/*
+* Emitting 1
+string 1
+Emitting 2
+Caught :: java.lang.IllegalStateException: Crashed on 2*/
+
