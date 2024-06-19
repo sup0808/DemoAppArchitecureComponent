25b3b9a81e82c9e7b690c4a7bda594fd6c23f215
supriya gupta <supriyag75@gmail.com>
2024-06-19 15:14:17 +0530

    zip() with list and set (just repalce mutableSetOf)

--- app/src/main/java/com/demoapparchitecurecomponent/flatMapExample.kt
+++ app/src/main/java/com/demoapparchitecurecomponent/flatMapExample.kt
@@ -3,13 +3,18 @@ package com.demoapparchitecurecomponent
 fun main() {
     // map() with mutableListOf--
 
-    var fruits = mutableListOf("mango", "garpes","lici","straberry")
-    var vegesarbles = mutableListOf("potato","onion","ginger","garlic")
+    var fruits = mutableListOf("A", "B","C","D")
+    var vege= mutableListOf(1,2,3,4)
 
-    var cart = listOf(fruits,vegesarbles)
-    var cartMap =  cart.map{it}
-    var cartFlatMap = cart.flatMap{it}
-    println(" Cart map $cartMap")
+
+    var cartMap =  fruits.zip(vege){ a,b->"$a--$b"}
+    var cartFlatMap =fruits zip  vege
+    println(cartMap)
     println(" Cart flatMap $cartFlatMap")
 
 }
+
+/*
+*
+* [A--1, B--2, C--3, D--4]
+ Cart flatMap [(A, 1), (B, 2), (C, 3), (D, 4)]*/
